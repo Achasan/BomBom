201c257d35b246baba01e1161b97ddedee524282
Achasan <79975278+Achasan@users.noreply.github.com>
2022-01-03 15:14:54 +0900

    TalkController test

---
+++ BomBom/src/main/java/com/bombom/controller/TalkController.java
@@ -0,0 +1,20 @@
+package com.bombom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TalkController {
+	
+	@RequestMapping(value="/user_content.do/{id}", method=RequestMethod.GET)
+	public String content(@PathVariable("id")String boardNo, Model model) {
+		
+		model.addAttribute("boardNo", boardNo);
+		
+		return "user/user_talk_content";
+	}
+	
+}
